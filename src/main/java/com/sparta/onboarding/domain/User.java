6cb6a99ee98a0022519264d1694b4a0afacea7dc
kyeonkim <kyeonkim5021@gmail.com>
2024-12-08 17:27:55 +0900

    update: 사용자 엔티티에 생성일시, 수정일시와 같은 감사 필드 추가

--- src/main/java/com/sparta/onboarding/domain/User.java
+++ src/main/java/com/sparta/onboarding/domain/User.java
@@ -3,17 +3,22 @@ package com.sparta.onboarding.domain;
 import com.sparta.onboarding.dto.UserRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "p_user")
@@ -21,6 +26,7 @@ import lombok.NoArgsConstructor;
 @Builder(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
   @Id
@@ -40,6 +46,14 @@ public class User {
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private UserRole role;
+
+   @CreatedDate
+   @Column(nullable = false, updatable = false)
+   private LocalDateTime createdAt;
+
+   @LastModifiedDate
+   @Column(nullable = false)
+   private LocalDateTime modifiedAt;
 
   public static User create(UserRequest.SignUp request, String encodedPassword) {
     return User.builder()
