3600c5665e73af45ccaca493fd1fc5bfe36e3bc2
kyeonkim <kyeonkim5021@gmail.com>
2024-12-08 17:14:52 +0900

    fix: Swagger Config - API 그룹화 설정 - 보안 스키마 설정 (JWT/OAuth2) - 상세한 API 설명

--- src/main/java/com/sparta/onboarding/config/SwaggerConfig.java
+++ src/main/java/com/sparta/onboarding/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.sparta.onboarding.config;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -13,13 +14,21 @@ public class SwaggerConfig {
   public OpenAPI openAPI() {
     return new OpenAPI()
         .components(new Components())
+        .components(new Components()
+          .addSecuritySchemes("bearer-key",
+            new SecurityScheme()
+              .type(SecurityScheme.Type.HTTP)
+              .scheme("bearer")
+              .bearerFormat("JWT")))
         .info(apiInfo());
   }
 
   private Info apiInfo() {
     return new Info()
-        .title("Onboarding API Test")
-        .description("Swagger UI")
+        .description("온보딩 프로젝트 API 문서\n\n" +
+            "## 주요 기능\n" +
+            "- 사용자 관리\n" +
+            "- 인증/인가\n")
         .version("1.0.0");
   }
 }
